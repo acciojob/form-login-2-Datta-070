4061b9da9cd55e48fc968a5e674e3a1011b70092
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-12-20 13:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById("myform").addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	var fname = document.getElementById('fname').value;
+	var lname = document.getElementById('lname').value;
+	var phone = document.getElementById('phone').value;
+	var mail = document.getElementById('mail').value;
+
+	var alertMessage = 'Name: ' + fname + '\nLast Name: ' + lname + '\nPhone no: ' + phone + '\nEmail: ' + mail;
+	alert(alertMessage);
+})
